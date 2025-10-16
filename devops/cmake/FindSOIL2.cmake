5e422377a698d34b0923b398bbb1f74d97f9ab1e
Joel Braun <joel22b@gmail.com>
2025-10-16 00:10:48 -0400

    SOIL2 dep test

--- devops/cmake/FindSOIL2.cmake
+++ devops/cmake/FindSOIL2.cmake
@@ -1,26 +1,32 @@
 include(FetchContent)
 
 FetchContent_Declare(
-    SOIL2
+    SOIL2-pkg
     GIT_REPOSITORY git@github.com:SpartanJ/SOIL2.git
     GIT_TAG 1.31
     GIT_SHALLOW TRUE
 )
 
-FetchContent_Populate(SOIL2)
-if (NOT soil2_NOT_POPULATED)
-    execute_process(
-        COMMAND cmake ${soil2_SOURCE_DIR}
-        WORKING_DIRECTORY ${soil2_SOURCE_DIR}
-    )
+FetchContent_MakeAvailable(SOIL2-pkg)
 
-    execute_process(
-        COMMAND make -j
-        WORKING_DIRECTORY ${soil2_SOURCE_DIR}
-    )
+set(SOIL2_INCLUDE_DIR "${soil2_SOURCE_DIR}/src")
 
-    set(SOIL2_INCLUDE_DIR "${soil2_SOURCE_DIR}/src")
-    set(SOIL2_LIBRARY "${soil2_SOURCE_DIR}/libsoil2.a")
+#add_subdirectory(${soil2_SOURCE_DIR} soil2-dir)
 
-    message(STATUS "Installed SOIL2 1.31")
-endif()
+#FetchContent_Populate(SOIL2)
+#if (NOT soil2_NOT_POPULATED)
+#    execute_process(
+#        COMMAND cmake ${soil2_SOURCE_DIR}
+#        WORKING_DIRECTORY ${soil2_SOURCE_DIR}
+#    )
+
+#    execute_process(
+#        COMMAND make -j
+#        WORKING_DIRECTORY ${soil2_SOURCE_DIR}
+#    )
+
+#    set(SOIL2_INCLUDE_DIR "${soil2_SOURCE_DIR}/src")
+#    set(SOIL2_LIBRARY "${soil2_SOURCE_DIR}/libsoil2.a")
+
+#    message(STATUS "Installed SOIL2 1.31")
+#endif()
